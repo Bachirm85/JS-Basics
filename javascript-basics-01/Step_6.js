c70a2a661f70021039731f802d809b9241ac58cb
Bachirm85 <mouawadbachir@gmail.com>
2021-05-28 19:54:07 +0300

    Step_6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,6 @@
+function validates() {
+    var myNum1 = document.getElementById("first_number").value;
+    var myNum2 = document.getElementById("second_number").value;
+
+    window.alert("result: " + myNum1 % myNum2);
+}
