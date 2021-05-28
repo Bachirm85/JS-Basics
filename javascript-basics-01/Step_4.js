225e7e460fb3c1a757e6eb7abdf1c23661b02670
Bachirm85 <mouawadbachir@gmail.com>
2021-05-28 19:40:23 +0300

    Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,7 @@
+function validates() {
+  var myName = document.getElementById("name").value;
+  var surname = document.getElementById("surname").value;
+  var city = document.getElementById("city").value;
+
+  window.alert("Name: " + myName + "\nSurname: " + surname + "\nCity: " + city);
+} 
