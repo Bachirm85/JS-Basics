9222f2ad73cdac5b24f5cc0574ccfa402fbae431
Bachirm85 <mouawadbachir@gmail.com>
2021-05-29 10:47:25 +0300

    Step_8

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,9 @@
+function validates(){
+    var myAge = document.getElementById("age").value;
+    if (myAge > 18) {
+        alert ("you are over 18");
+      } else {
+        alert ("you are under 18");
+      }
+    
+}
