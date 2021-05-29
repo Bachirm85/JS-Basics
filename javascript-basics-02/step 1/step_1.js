c173beb8efd681c27df5c9a85462a69bf32ab98e
Bachirm85 <mouawadbachir@gmail.com>
2021-05-29 14:39:45 +0300

    step_1

---
+++ javascript-basics-02/step 1/step_1.js
@@ -0,0 +1,14 @@
+// I understand better than query selector
+function setNewImage() {
+    document.getElementById("image1").src ="images/image1_2.jpg";
+}
+function setOldImage() {
+    document.getElementById("image1").src ="images/image1.jpg";
+}
+
+// need more practice on that
+var img = document.querySelector("#image2");
+
+img.addEventListener("mouseover",() => {
+    img.setAttribute("src", "images/image1_2.jpg");
+});
